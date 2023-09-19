e19cc0943e9273582ea4eb9f04b00343818964f0
Musyoki-Wambua <wjosephmusyoki@gmail.com>
2023-09-19 21:39:33 +0300

    Corrected Code

--- print_functions.c
+++ print_functions.c
@@ -40,7 +40,7 @@ int print_str(va_list arg)
  * Return: 0
  */
 
-int print_int(va_list arg)
+int print_unsigned(va_list arg)
 {
 	unsigned int divisor = 1, i, resp, charPrinted = 0;
 	int n = va_arg(arg, int);
@@ -51,15 +51,51 @@ int print_int(va_list arg)
 		charPrinted++;
 		n *= -1;
 	}
+    int divisor = 1, i, resp;
+	unsigned int n = va_arg(arg, unsigned int);
 
 	for (i = 0; n / divisor > 9; i++, divisor *= 10)
 		;
 
-	for (; divisor >= 1; n %= divisor, divisor /= 10, charPrinted++)
+	for (; divisor >= 1; n %= divisor, divisor /= 10)
 	{
 		resp = n / divisor;
 		_putchar('0' + resp);
 	}
 
-	return (charPrinted);
+	return (i + 1);
+}
+
+/**
+ * print_STR - prints a string with a `S` (upper case) specificer
+ * @arg: argument
+ * Return: number of character printed
+ */
+
+int print_STR(va_list arg)
+{
+	int i;
+	char *str = va_arg(arg, char*);
+
+	if (str == NULL)
+		str = "(null)";
+	else if (*str == '\0')
+		return (-1);
+
+	for (i = 0; str[i]; i++)
+	{
+		if ((str[i] < 32 && str[i] > 0) || str[i] >= 127)
+		{
+			_putchar('\\');
+			_putchar('x');
+			if (i < 16)
+				_putchar('0');
+
+			print_unsignedIntToHex(str[i], 'A');
+		}
+		else
+			_putchar(str[i]);
+	}
+
+	return (i);
 }
